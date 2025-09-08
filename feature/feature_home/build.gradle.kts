8119fa9777c51cd76c52b2ac9dce890cfc8c3ad0
alehkastsiukovich <alehkastsiukovich@gmail.com>
2025-09-09 00:19:55 +0200

    wip

--- feature/feature_home/build.gradle.kts
+++ feature/feature_home/build.gradle.kts
@@ -5,11 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.composeHotReload)
+    id("multiplatformConvention")
 }
 
 kotlin {
@@ -77,33 +73,6 @@ kotlin {
             implementation(compose.desktop.currentOs)
             implementation(libs.kotlinx.coroutinesSwing)
         }
-    }
-}
-
-android {
-    namespace = "org.vocabulary.pulse"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    defaultConfig {
-        applicationId = "org.vocabulary.pulse"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
---
+++ multiplatform-convention-plugin/src/main/kotlin/multiplatformConvention.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("com.android.library")
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    kotlin("plugin.compose")
+}
